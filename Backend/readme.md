c39a3d547f1a54c7057af2e52f3c00eb66751304
Julien <19209856+FLOKITOTO@users.noreply.github.com>
2023-03-24 16:01:44 +0100

    fixe RESTFUL api

---
+++ Backend/readme.md
@@ -0,0 +1,267 @@
+# DOCUMENTATION API RESTFUL
+
+Système simple de base de données (SGBD) en Nodejs sans aucun framework avec des BASES DE DONNEES IN MEMORY ainsi q'un stockage péreen asynchrone, les données ne doivent pas être perdues en cas d'arrêt.
+Le système doit être RESTFUL. Toutes les interactions avec la base de données se font via des appels de web services avec des commandes curl.
+Voici l'organisation des données en base avec des tables et les différents PATH.
+
+## Sommaire
+
+1. [Consignes](#consignes)
+2. [Pré-requis](#pré-requis)
+   - [Dépendances](#dépendances)
+3. [Utilisations](#utilisations)
+
+4. [Exemples d&#39;utilisations](#exemples-dutilisations)
+
+5. [Licence](#licence)
+
+---
+
+## Consignes
+
+Démarrez votre environnement visual studio code, un premier terminal et accéder à la racine http://localhost:3000/. Ensuite vous pouvez créer une base de donnée pour cela effectuer la commande curl -X ### ACCES AUX BASES DE DONNÉES
+
+[Sommaire](#sommaire)
+
+---
+
+## Pré-requis
+
+L'installation de **[NodeJs](https://nodejs.org/en)** est recommandé pour l'éxécution du script
+
+[Sommaire](#sommaire)
+
+---
+
+## Dépendances
+
+- [fs](https://nodejs.org/api/fs.html),
+- [http](https://nodejs.org/api/http.html),
+
+## [Sommaire](#sommaire)
+
+## Utilisations
+
+### **RACINE**
+
+```
+/
+```
+
+`GET` **Récupère les accès à databases et help**
+
+Réponses :
+
+- `200 OK` : la racine de l'API a été atteinte avec succès
+- `404 Not Found` : la racine de l'API n'existe pas
+
+La racine est le point d'entré principal avec comme propositions /databases & /help.
+
+---
+
+### AIDE
+
+```
+/help
+```
+
+`GET` **Accéder aux commandes curls**
+
+Réponses :
+
+- `200 OK` : la documentation de l'API
+- `404 Not Found` : la documentation de l'API n'existe pas
+
+La documentation va détailler l'ensemble des commandes curl disponible.
+
+---
+
+### ACCES AUX BASES DE DONNÉES
+
+```
+/databases
+```
+
+`GET` **Lister toutes les bases de données**
+
+Réponses :
+
+- `200 OK` : la liste de toutes les bases de données existantes
+
+Cette commande permet de lister l'ensemble des bases de données disponible.
+
+---
+
+`POST` **Créer une BDD**
+
+_BODY REQUEST :_
+
+```
+{
+  "id": ":database_id"
+  "name": ":database_name"
+}
+```
+
+Réponses :
+
+- `201 Created` : la base de données a été créée avec succès
+- `409 Conflict` : la base de données existe déjà
+- `400 Bad Request` : Le corps de la requête est mal formé ou des paramètres obligatoires sont manquants.
+
+Cette commande permet de créer une base de données, avec un corps de requête `name`.
+
+---
+
+### ACTION DEPUIS UNE BASE DE DONNEES
+
+```
+/databases/:database_name
+```
+
+`GET` **Lister toutes les tables d'une BDD**
+
+Réponses :
+
+- `200 OK` : la liste de toutes les tables de la base de données spécifiée
+- `404 Not Found` : la base de données n'existe pas
+
+Cette commande permet de lister l'ensemble des des tables d'une base de données.
+
+---
+
+`POST` **Créer une table dans une BDD**
+
+_BODY REQUEST :_
+
+```
+{
+  "name": ":table_name"
+}
+```
+
+Réponses :
+
+- `201 Created` : la base de données a été créée avec succès
+- `409 Conflict` : la base de données existe déjà
+- `400 Bad Request` : Le corps de la requête est mal formé ou des paramètres obligatoires sont manquants.
+
+Cette commande permet de créer une table dans une base de données.
+
+---
+
+**`DELETE` Supprimer une BDD**
+
+Réponses :
+
+- `200 OK` : la base de données a été supprimée avec succès
+- `404 Not Found` : la base de données n'existe pas
+
+Cette commande permet de supprimer une base de données.
+
+Du point d'une API RESTFUL nous devons supprimer une base de données depuis sa ressource (/databases/:database_name).
+
+---
+
+### ACCES AUX DONNEES
+
+```
+/databases/:database_name/:table_name
+```
+
+`GET` **Lister tous les champs d'une table**
+
+Réponses :
+
+- `200 OK` : La liste de toutes les données de la table
+- `404 Not Found` : La base de données ou la table n'existe pas
+
+Cette commande permet de lister tous les champs d'une table.
+
+---
+
+`POST` **Insérer des données dans une table**
+
+_BODY REQUEST_
+
+```
+{
+  "key": "value",
+  "key": value,
+  "key": "value"
+}
+```
+
+Réponses :
+
+- `201 Created` : Les données ont été insérées avec succèsµ
+- `404 Not Found` : La base de données ou la table n'existe pas
+- `400 Bad Request` : Le corps de la requête est mal formé ou des paramètres obligatoires sont manquants.
+
+Cette commande permet d'insérer des données dans une table.
+
+---
+
+### ACTION DEPUIS UNE TABLE
+
+```
+/databases/:database_name/:table_name/:id
+```
+
+`PUT` Mettre à jour une donnée
+
+```
+{
+  "key": "value",
+  "key": value,
+  "key": "value"
+}
+```
+
+Réponses :
+
+- `200 OK` : La donnée a été mise à jour avec succès
+- `404 Not Found` : La base de données, la table ou l'identifiant de la donnée n'existe pas
+- `400 Bad Request` : Le corps de la requête est mal formé ou des paramètres obligatoires sont manquants.
+
+Cette commande permet de mettre à jour des données via un ID.
+
+---
+
+**`DELETE` Supprimer une donnée depuis une table avec un ID:**
+
+Réponses :
+
+- `200 OK` : la donnée a été supprimée avec succès
+- `404 Not Found` : la base de données, la table ou l'identifiant de la donnée n'existent pas
+
+Cette commande permet de supprimer une donnée via une table
+
+---
+
+// pas encore fait
+
+Supprimer plusieurs données
+
+```
+DELETE /databases/:database_name/:table_name?colonne1=valeur1
+
+```
+
+[Sommaire](#sommaire)
+
+## Exemples-dutilisations
+
+![aperçu](preview.gif)
+
+[Sommaire](#sommaire)
+
+## Licence
+
+Code sous license [GPL v3](LICENSE)
+
+## Authors
+
+[FLOKITOTO](https://github.com/FLOKITOTO)
+[YOUTUBE](https://www.youtube.com/channel/UCQpPEJYhZn-PW3Ss0iIvPfQ)
+[TWITTER](https://twitter.com/Fractalys_Sync)
--- Backend/server.js
+++ Backend/server.js
@@ -1,235 +1,266 @@
 const http = require("http");
 const fs = require("fs");
 
-// Initialisation des bases de données vides
+// Load databases from JSON file
 let databases = {};
 
-// Création du serveur HTTP
 const server = http.createServer((req, res) => {
-  // Parsing de l'URL de la requête
-  const urlParts = req.url.split("/").filter((part) => part !== "");
-
-  // Récupération des noms de base de données, de table et d'identifiant (si présent)
-  const databaseName = urlParts[0];
-  const tableName = urlParts[1];
-  const id = urlParts[2];
-
-  // Help
-  const validEndpoints = [
-    {
-      title: "CREATE a BASE ⚡",
-      term: "curl -X `method` `url`",
-      method: "POST",
-      url: "http://localhost:3000/:databaseName",
-    },
-    {
-      title: "CREATE a TABLE ⚡",
-      term: "curl -X `method` `url`",
-      method: "POST",
-      url: "http://localhost:3000/:databaseName/:tableName",
-    },
-    {
-      title: "INSERT DATA in a TABLE ⚡",
-      term: "curl -X `method` -H `contentType` -d `bodyRequest` `url`",
-      contentType: "Content-Type: application/json",
-      bodyRequest: '\'{"id": x, "name": "x"}\'',
-      method: "POST",
-      url: "http://localhost:3000/:databaseName/:tableName",
-    },
-    {
-      title: "GET DATA from TABLE 👀",
-      term: "curl -X `method` `url`",
-      method: "GET",
-      url: "http://localhost:3000/:databaseName/:tableName",
-    },
-    {
-      title: "GET DATA from ONE FIELD by ID 👀",
-      term: "curl -X `method` `url`",
-      method: "GET",
-      url: "http://localhost:3000/:databaseName/:tableName:/:id",
-    },
-    {
-      title: "UPDATE DATA from ONE FIELD by ID ✅",
-      term: "curl -X `method` -H `contentType` -d `bodyRequest` `url`",
-      contentType: "Content-Type: application/json",
-      bodyRequest: '\'{"id": x, "name": "x"}\'',
-      method: "PUT",
-      url: "http://localhost:3000/:databaseName/:tableName:/:id",
-    },
-    {
-      title: "DELETE ONE FIELD by ID ❌",
-      term: "curl -X `method` `url`",
-      method: "DELETE",
-      url: "http://localhost:3000/:databaseName/:tableName:/:id",
-    },
-    {
-      title: "DELETE TABLE in DATABASE ❌",
-      term: "curl -X `method` `url`",
-      method: "DELETE",
-      url: "http://localhost:3000/:databaseName/:tableName:",
-    },
-    {
-      title: "DELETE DATABASE ❌",
-      term: "curl -X `method` `url`",
-      method: "DELETE",
-      url: "http://localhost:3000/:databaseName",
-    },
-    {
-      title: "See all commands 👀",
-      term: "curl -X `method` `url`",
-      method: "GET",
-      url: "http://localhost:3000/help",
-    },
-  ];
-
-  const validEndpoint = validEndpoints.find(
-    (endpoint) => endpoint.method === req.method && req.url.match(endpoint.url)
-  );
-
-  if (validEndpoint) {
-    // Handle valid request
-    console.log(validEndpoints);
-  } else {
-    console.log(
-      "This command does not exist. Type /help for a list of available commands."
-    );
-  }
-
-  // Gestion de la création d'une base de données
-  if (req.method === "POST" && databaseName && !tableName && !id) {
-    if (!databases[databaseName]) {
-      databases[databaseName] = {};
-      saveDatabases();
-      res.writeHead(201);
-      res.end("Database created successfully");
-    } else {
-      res.writeHead(400);
-      res.end("Database already exists");
+  if (req.url === "/") {
+    // ROOT du projet
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Hello World!\n");
+  } else if (req.url === "/databases") {
+    // GET POST /databases
+    if (req.method === "GET") {
+      // Listes BDD
+      const databaseList = Object.keys(databases);
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(databaseList));
+    } else if (req.method === "POST") {
+      // Création BDD
+      let body = "";
+      req.on("data", (chunk) => {
+        body += chunk.toString();
+      });
+      req.on("end", () => {
+        const databaseName = JSON.parse(body).name;
+        databases[databaseName] = {};
+        saveDatabases(databases);
+        res.writeHead(201, { "Content-Type": "application/json" });
+        res.end(
+          JSON.stringify({
+            message: `Database ${databaseName} created successfully`,
+          })
+        );
+      });
     }
-  }
-
-  // Gestion de la suppression d'une base de données
-  else if (req.method === "DELETE" && databaseName && !tableName && !id) {
+  } else if (req.url.startsWith("/databases/")) {
+    // Handle requests for specific databases
+    const urlParts = req.url.split("/");
+    const databaseName = urlParts[2];
     if (databases[databaseName]) {
-      delete databases[databaseName];
-      saveDatabases();
-      res.writeHead(200);
-      res.end("Database deleted successfully");
-    } else {
-      res.writeHead(400);
-      res.end("Database does not exist");
-    }
-  }
-
-  // Gestion de la création d'une table dans une base de données
-  else if (req.method === "POST" && databaseName && tableName && !id) {
-    if (!databases[databaseName][tableName]) {
-      databases[databaseName][tableName] = {};
-      saveDatabases();
-      res.writeHead(201);
-      res.end("Table created successfully");
-    } else {
-      res.writeHead(400);
-      res.end("Table already exists");
-    }
-  }
-
-  // Gestion de la suppression d'une table dans une base de données
-  else if (req.method === "DELETE" && databaseName && tableName && !id) {
-    if (databases[databaseName][tableName]) {
-      delete databases[databaseName][tableName];
-      saveDatabases();
-      res.writeHead(200);
-      res.end("Table deleted successfully");
-    } else {
-      res.writeHead(400);
-      res.end("Table does not exist");
-    }
-  }
-
-  // Gestion de la récupération de toutes les données d'une table
-  else if (req.method === "GET" && databaseName && tableName && !id) {
-    if (databases[databaseName][tableName]) {
-      res.writeHead(200);
-      res.end(JSON.stringify(databases[databaseName][tableName]));
-    } else {
-      res.writeHead(400);
-      res.end("Table does not exist");
-    }
-  }
-
-  // Gestion de la récupération d'une donnée par son id
-  else if (req.method === "GET" && databaseName && tableName && id) {
-    if (
-      databases[databaseName][tableName] &&
-      databases[databaseName][tableName][id]
-    ) {
-      res.writeHead(200);
-      res.end(JSON.stringify(databases[databaseName][tableName][id]));
-    } else {
-      res.writeHead(400);
-      res.end("Data does not exist");
-    }
-  }
-
-  // Gestion de l'ajout ou de la mise à jour d'une donnée par son id
-  else if (req.method === "PUT" && databaseName && tableName && id) {
-    let body = "";
-
-    req.on("data", (chunk) => {
-      body += chunk.toString();
-    });
-
-    req.on("end", () => {
-      let data;
-
-      try {
-        data = JSON.parse(body);
-      } catch (error) {
-        res.writeHead(400);
-        res.end("Invalid JSON");
-        return;
+      if (urlParts.length === 3) {
+        if (req.method === "GET") {
+          // GET /databases/:database_name
+          // Listes des tables d'une BDD
+          const tableList = Object.keys(databases[databaseName]);
+          res.writeHead(200, { "Content-Type": "application/json" });
+          res.end(JSON.stringify(tableList));
+        } else if (req.method === "DELETE") {
+          // DELETE /databases/:database_name
+          // Supression d'une BDD
+          delete databases[databaseName];
+          saveDatabases(databases);
+          res.writeHead(200, { "Content-Type": "application/json" });
+          res.end(
+            JSON.stringify({
+              message: `Database ${databaseName} deleted successfully`,
+            })
+          );
+        } else if (req.method === "POST") {
+          // POST /databases/:database_name
+          // Création d'une table dans une BDD
+          let body = "";
+          req.on("data", (chunk) => {
+            body += chunk.toString();
+          });
+          req.on("end", () => {
+            const tableName = JSON.parse(body).name;
+            databases[databaseName][tableName] = [];
+            saveDatabases(databases);
+            res.writeHead(201, { "Content-Type": "application/json" });
+            res.end(
+              JSON.stringify({
+                message: `Table ${tableName} created successfully in ${databaseName}`,
+              })
+            );
+          });
+        }
+      } else if (urlParts.length === 4) {
+        const tableName = urlParts[3];
+        if (databases[databaseName][tableName]) {
+          if (req.method === "GET") {
+            // GET /databases/:database_name/:table_name
+            // Listes des tables d'une BDD
+            const fields = Object.keys(
+              databases[databaseName][tableName][0]
+            ).join(", ");
+            const data = databases[databaseName][tableName].map((obj) =>
+              Object.values(obj)
+            );
+            res.writeHead(200, { "Content-Type": "application/json" });
+            res.end(
+              JSON.stringify({
+                table_name: tableName,
+                fields: fields,
+                data: data,
+              })
+            );
+          } /*ici */ else if (req.method === "POST") {
+            // POST /databases/:database_name/:table_name
+            // Insertion de données dans une table
+            let body = "";
+            req.on("data", (chunk) => {
+              body += chunk.toString();
+            });
+            req.on("end", () => {
+              try {
+                const row = JSON.parse(body);
+                const table = databases[databaseName][tableName];
+                const id = table.length + 1;
+                row.id = id;
+                table.push(row);
+                saveDatabases();
+                res.writeHead(201, { "Content-Type": "application/json" });
+                res.end(
+                  JSON.stringify({
+                    id: id,
+                    message: "Data inserted successfully",
+                  })
+                );
+              } catch (error) {
+                res.writeHead(400, { "Content-Type": "application/json" });
+                res.end(
+                  JSON.stringify({
+                    error: "Invalid JSON payload",
+                  })
+                );
+              }
+            });
+          } else if (req.method === "DELETE") {
+            // DELETE /databases/:database_name/:table_name
+            // Suppression d'une table
+            delete databases[databaseName][tableName];
+            saveDatabases(databaseName);
+            res.writeHead(200, { "Content-Type": "text/plain" });
+            res.end("Table deleted successfully");
+          } else {
+            res.writeHead(404, { "Content-Type": "text/plain" });
+            res.end("Endpoint not found");
+          }
+        } else {
+          res.writeHead(404, { "Content-Type": "application/json" });
+          res.end(
+            JSON.stringify({
+              error: `Table ${tableName} not found in ${databaseName} database`,
+            })
+          );
+        }
+      } else if (urlParts.length === 5) {
+        const tableName = urlParts[3];
+        const id = parseInt(urlParts[4]);
+        if (databases[databaseName][tableName]) {
+          const table = databases[databaseName][tableName];
+          const row = table.find((obj) => obj.id === id);
+          if (row) {
+            if (req.method === "GET") {
+              // GET /databases/:database_name/:table_name/:id
+              // Listes des champs d'une table
+              const fields = Object.keys(row).join(", ");
+              res.writeHead(200, { "Content-Type": "application/json" });
+              res.end(
+                JSON.stringify({
+                  fields: fields,
+                  data: row,
+                })
+              );
+            } else if (req.method === "PUT") {
+              // PUT /databases/:database_name/:table_name/:id
+              // Mise à jour d'une donnée
+              let body = "";
+              req.on("data", (chunk) => {
+                body += chunk.toString();
+              });
+              req.on("end", () => {
+                try {
+                  const newValues = JSON.parse(body);
+                  const keys = Object.keys(newValues);
+                  keys.forEach((key) => {
+                    row[key] = newValues[key];
+                  });
+                  saveDatabases(databases); // Sauvegarde des modifications dans databases.json
+                  res.writeHead(200, { "Content-Type": "application/json" });
+                  res.end(
+                    JSON.stringify({
+                      message: "Data updated successfully",
+                    })
+                  );
+                } catch (error) {
+                  res.writeHead(400, { "Content-Type": "application/json" });
+                  res.end(
+                    JSON.stringify({
+                      error: "Invalid JSON payload",
+                    })
+                  );
+                }
+              });
+            } else if (req.method === "DELETE") {
+              // DELETE /databases/:database_name/:table_name/:id
+              // Supprimer une donnée
+              const index = table.findIndex((obj) => obj.id === id);
+              table.splice(index, 1);
+              saveDatabases(databaseName);
+              res.writeHead(200, { "Content-Type": "application/json" });
+              res.end(JSON.stringify(table));
+            } else {
+              res.writeHead(404, { "Content-Type": "text/plain" });
+              res.end("Endpoint not found");
+            }
+          } else {
+            res.writeHead(404, { "Content-Type": "text/plain" });
+            res.end("Endpoint not found");
+          }
+        } else {
+          res.writeHead(404, { "Content-Type": "text/plain" });
+          res.end("Endpoint not found");
+        }
       }
-
-      if (!databases[databaseName][tableName]) {
-        res.writeHead(400);
-        res.end("Table does not exist");
-        return;
-      }
-
-      databases[databaseName][tableName][id] = data;
-      saveDatabases();
-      res.writeHead(200);
-      res.end("Data saved successfully");
-    });
-  }
-
-  // Gestion de la suppression d'une donnée par son id
-  else if (req.method === "DELETE" && databaseName && tableName && id) {
-    if (
-      databases[databaseName][tableName] &&
-      databases[databaseName][tableName][id]
-    ) {
-      delete databases[databaseName][tableName][id];
-      saveDatabases();
-      res.writeHead(200);
-      res.end("Data deleted successfully");
-    } else {
-      res.writeHead(400);
-      res.end("Data does not exist");
     }
-  }
-
-  // Gestion de toutes les autres requêtes
-  else {
-    res.writeHead(404);
-    res.end("Endpoint not found");
   }
 });
+
+function loadDatabases() {
+  try {
+    const data = fs.readFileSync("./databases.json");
+    databases = JSON.parse(data);
+    const databaseCount = Object.keys(databases).length;
+    if (databaseCount === 0) {
+      console.log("\x1b[31mNo existing databases\x1b[0m");
+    } else {
+      console.log(
+        `\x1b[32m${databaseCount} database(s) found in databases.json:\x1b[0m`
+      );
+      for (const databaseName in databases) {
+        console.log(`\x1b[33m${databaseName}:\x1b[0m`);
+        for (const tableName in databases[databaseName]) {
+          console.log(`  - \x1b[34m${tableName}:\x1b[0m`);
+          const tableData = databases[databaseName][tableName];
+          if (tableData.length > 0) {
+            console.table(tableData);
+          } else {
+            console.log("    No data found in table.");
+          }
+        }
+      }
+    }
+  } catch (error) {
+    console.log("\x1b[31mNo existing databases\x1b[0m");
+  }
+}
+
+// Save databases to file
+function saveDatabases() {
+  try {
+    const data = JSON.stringify(databases);
+    fs.writeFileSync("./databases.json", data);
+  } catch (err) {
+    console.error(`Error saving databases: ${err}`);
+  }
+}
 
 // Démarrage du serveur HTTP
 const port = 3000;
 server.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+  console.log(`Server running at http://localhost:${port}/`);
   loadDatabases();
 });
